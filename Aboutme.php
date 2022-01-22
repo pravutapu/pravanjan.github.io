1f34c08c02dda38b461cc8da289a77a6eea2bc5d
Pravanjan Nayak <93814121+pravutapu@users.noreply.github.com>
2022-01-22 22:45:23 +0530

    Add files via upload

---
+++ Aboutme.php
@@ -0,0 +1,97 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">    
+    <link rel="stylesheet" href="assets/css/owl.theme.default.min.css">    
+    <link rel="stylesheet" href="assets/css/menuStyles.css">    
+    <link rel="stylesheet" href="assets/css/customeStyle.css">
+    <link rel="stylesheet" href="assets/css/responsiveStyle.css">
+    <title>CENTRE OF EXCELLENCE ON ADVANCED MANUFACTURING TECHNOLOGIES</title>
+  </head>
+  <body>
+  
+<?php include('header.php');?>
+   
+    <section class="insideBanner">
+    	<img src="assets/images/insidebanner.jpg" class="img-fluid">
+    </section>
+    
+    
+    <section class="insideBodypart">
+    	<div class="container-fluid main-container">
+    		<h1>About the Centre</h1>
+    		<p>The Centre of Excellence in Advanced Manufacturing Technology has been established at IIT Kharagpur through the support of the <a href="https://dhi.nic.in/" target="_blank"><strong>Department of Heavy Industry of Ministry of Heavy Industries and Public Enterprises, Government of India,</strong></a> along with a consortium of top industry members in the country. The centre aims to stimulate the innovation to manufacture smart machines in the capital goods sector. The center will bring together various industries in this area to work in a synergistic way towards the common goals of infusing cutting edge technologies, and to come up with research and development for sustainable products having higher productivity with reduced cost.</p>
+    		<p>This centre offers a unique platform for collaborative, consortium driven infusion of advanced technologies in the manufacturing area, which is in harmony with the <strong>'Make-in-India'</strong> initiative of the Government of India. The centre will initiates innovative and top-quality research focused to the industries on <strong><em>Specialty materials, Design and automation, Additive manufacturing,</em></strong> and <strong><em>Digital manufacturing and Industrial Internet of Things.</em></strong> The centre will boost innovative interventions in the advanced manufacturing domain by enabling an ecosystem among Institutes of higher repute, heavy industries, and also the MSMEs and start-ups. The centre looks for active participation in this ecosystem for a collaborative research in the proposed areas. </p>
+    		<p>The centre also houses an <strong><em>Innovation Lab</em></strong> to facilitate the culture of innovation and open engineering. The Innovation Lab invites MSMEs and the Start-ups to grab opportunities of getting an end-to-end support from the experts including access to various state-of-the-art facilities for early prototyping of their product. The centre also welcomes bright and talented scholars with high value doctoral fellowship to support its activities.</p>
+    		
+    		<section class="insideBodypart">
+    	<div class="container-fluid main-container">
+    		<h1>History</h1>
+    		<!--<p>The Centre of Excellence in Advanced Manufacturing Technology has been established at IIT Kharagpur through the support of the <a href="https://dhi.nic.in/" target="_blank"><strong>Department of Heavy Industry of Ministry of Heavy Industries and Public Enterprises, Government of India,</strong></a> along with a consortium of top industry members in the country. The centre aims to stimulate the innovation to manufacture smart machines in the capital goods sector. The center will bring together various industries in this area to work in a synergistic way towards the common goals of infusing cutting edge technologies, and to come up with research and development for sustainable products having higher productivity with reduced cost.</p>
+    		<p>This centre offers a unique platform for collaborative, consortium driven infusion of advanced technologies in the manufacturing area, which is in harmony with the <strong>'Make-in-India'</strong> initiative of the Government of India. The centre will initiates innovative and top-quality research focused to the industries on <strong><em>Specialty materials, Design and automation, Additive manufacturing,</em></strong> and <strong><em>Digital manufacturing and Industrial Internet of Things.</em></strong> The centre will boost innovative interventions in the advanced manufacturing domain by enabling an ecosystem among Institutes of higher repute, heavy industries, and also the MSMEs and start-ups. The centre looks for active participation in this ecosystem for a collaborative research in the proposed areas. </p>
+    		<p>The centre also houses an <strong><em>Innovation Lab</em></strong> to facilitate the culture of innovation and open engineering. The Innovation Lab invites MSMEs and the Start-ups to grab opportunities of getting an end-to-end support from the experts including access to various state-of-the-art facilities for early prototyping of their product. The centre also welcomes bright and talented scholars with high value doctoral fellowship to support its activities.</p>-->
+    		
+
+   		
+    		
+    		<hr/>
+    	<div class="row">
+    		<div class="col-lg-6">
+    			<h2 class="text-center mb-3 mt-5">Ecosystem of the Centre</h2>
+    			<img src="assets/images/DHI_ecosystem_centre.jpg" class="img-fluid">
+    			
+    		</div>
+    		<div class="col-lg-6">
+    		<h2 class="text-center mb-3 mt-5">Activities of the Centre </h2>
+    		<img src="assets/images/DHI_Activities_centre.jpg" class="img-fluid">
+    		</div>
+    	</div>
+    	</div>
+    </section>
+    
+    
+    
+        
+   <?php include('footer.php');?>
+    
+  
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/popper.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/owl.carousel.js"></script>
+<script src="assets/js/script.js"></script>
+<script src="assets/js/navigation_menu.js"></script>
+ <script>
+	$(function(){
+		$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+	autoplay:true,
+	dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+	})
+</script>
+  </body>
+</html>
